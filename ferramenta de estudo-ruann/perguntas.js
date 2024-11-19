b508765b23613b71d33e40634dc7e73bd622b833
Ruannteruaki <00001081825911SP@al.educacao.sp.gov.br>
2024-11-18 21:58:32 -0300

    Add files via upload

---
+++ ferramenta de estudo-ruann/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é html?',
+    'O html é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?',
+    'A capital da França é Roma'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'tchau em ingles é BYE (BAI)'
+)
